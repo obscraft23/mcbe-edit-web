0bae9fd47e50d11a6c8f6372833b6521b7e1c673
obscraft23 <obscraft23@gmail.com>
2023-03-24 01:14:45 +0900

    initial commit

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+apt-get install cmake
+pip install Django==4.1.7
 pip install scikit-build==0.16.7
 pip install django-jsoneditor== 0.2.4
 pip install numpy==1.24.2
-pip install scikit-build==0.16.7
 pip install pybedrock==0.0.3
 python3.9 manage.py collectstatic
