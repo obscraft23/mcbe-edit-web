57d29f697ee3b2380245ab4d4c77bb16797b54e2
obscraft23 <obscraft23@gmail.com>
2023-03-24 18:49:00 +0900

    initial commit

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,5 @@
 pip install Django==4.1.7
 pip install django-jsoneditor==0.2.4
 pip install numpy==1.24.2
-pip install pybedrock==0.0.3
+pip install pybedrock==0.0.5
 python3.9 manage.py collectstatic
