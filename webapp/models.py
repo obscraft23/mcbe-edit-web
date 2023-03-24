d3d0239473d781987ce70999191b507e779a4210
obscraft23 <obscraft23@gmail.com>
2023-03-25 01:19:47 +0900

    v0.0.1

--- webapp/models.py
+++ webapp/models.py
@@ -1,9 +1,1 @@
 from django.db import models
-#from jsoneditor.fields.django3_jsonfield import JSONField
-#from django.db.models.fields.json import JSONField
-from jsoneditor.fields.django3_jsonfield import JSONField
-
-from .jsonwidget import JSONField as JSONField2
-
-import json
-import os
