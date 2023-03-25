93d8d1935a7afc8daebf1a168a04756c6ce77344
obscraft23 <obscraft23@gmail.com>
2023-03-25 22:29:06 +0900

    v0.0.1

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,6 @@ ALLOWED_HOSTS = [
 INSTALLED_APPS = [
     'jsoneditor',
     'webapp',
-    'analytical',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
