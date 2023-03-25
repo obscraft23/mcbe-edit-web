4c48b83d8cd38a7c8c13ca61d06b5262e7839de8
obscraft23 <obscraft23@gmail.com>
2023-03-25 22:28:04 +0900

    v0.0.1

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,4 @@ pip install Django==4.1.7
 pip install django-jsoneditor==0.2.4
 pip install numpy==1.24.2
 pip install pybedrock==0.0.5
-pip install django-analytical
 python3.9 manage.py collectstatic
--- config/settings.py
+++ config/settings.py
@@ -38,13 +38,14 @@ ALLOWED_HOSTS = [
 INSTALLED_APPS = [
     'jsoneditor',
     'webapp',
+    'analytical',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'analytical',
+
 ]
 
 MIDDLEWARE = [
@@ -70,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'webapp.context_processors.google_analytics',
             ],
         },
     },
@@ -142,4 +144,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static/'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static/')
 
-GOOGLE_ANALYTICS_GTAG_PROPERTY_ID = "G-BC2S1FGB8V"
+GOOGLE_ANALYTICS_TRACKING_ID = "G-BC2S1FGB8V"
