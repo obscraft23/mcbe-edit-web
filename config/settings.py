3566f4df72b96f6cd6b834c4abdc684502d37c35
obscraft23 <123219490+obscraft23@users.noreply.github.com>
2023-04-22 08:34:50 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1rs+d&kr#cyfw(ca0u1gunnerir(94iap1_gn2$^u7-zri8wvi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     #'*',
